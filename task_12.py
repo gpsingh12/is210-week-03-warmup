e12e1b86c88d5289a9f3727a41409050252b6df8
gpsingh12 <gurpreet_1441@yahoo.com>
2015-02-13 02:49:18 +0000

    variables

---
+++ task_12.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Docstring"""
+INTVAL = 1
+FLOATVAL  = 0.1
+import decimal
+DECVAL = 1/float(10)
+import fractions
+FRACVAL = fractions.Fraction(numerator =1, denominator =10)
