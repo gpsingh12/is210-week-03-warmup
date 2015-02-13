35086a4179289328c8c9c51485070f5e7929046b
gpsingh12 <gurpreet_1441@yahoo.com>
2015-02-13 07:15:50 +0000

    variables assigned

--- task_12.py
+++ task_12.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 """Docstring"""
 INTVAL = 1
-FLOATVAL  = 0.1
+FLOATVAL = 0.1
 import decimal
-DECVAL = 1/float(10)
+DECVAL = decimal.Decimal('0.1')
 import fractions
-FRACVAL = fractions.Fraction(numerator =1, denominator =10)
+FRACVAL = fractions.Fraction(1, 10)
