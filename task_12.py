848dff93a81130746eb1112c3c5914b6af4f5360
gpsingh12 <gurpreet_1441@yahoo.com>
2015-02-14 02:49:06 +0000

    var

--- task_12.py
+++ task_12.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Docstring"""
+"""Docstring."""
+FLOATVAL = ("0.1")
 INTVAL = 1
-FLOATVAL = 0.1
 import decimal
-DECVAL = decimal.Decimal('0.1')
+DECVAL = decimal.Decimal("0.1")
 import fractions
-FRACVAL = fractions.Fraction(1, 10)
+FRACVAL = fractions.Fraction("1/10")
