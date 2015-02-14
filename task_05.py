ec2295ce1711f3cf6f0e5054fe39a4bf36dcdef8
gpsingh12 <gurpreet_1441@yahoo.com>
2015-02-14 01:32:46 +0000

    split

--- task_05.py
+++ task_05.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Splinter would be proud."""
-
-
 TEENAGE_MUTANT_NINJAS = ('Michaelangelo. Leonardo. Rafael. Donatello. Heroes '
-                         'in a half shell.')
+'in a half shell.')
+TURTLE_POWER =TEENAGE_MUTANT_NINJAS.split('. ')
